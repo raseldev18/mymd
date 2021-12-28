7a9abcc02a750fe4a8e440319f43bdc7a0724534
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-12-28 21:39:52 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -36,22 +36,6 @@ module.exports = {
       year: 'numeric'
     }).format(d)
     let _uptime = process.uptime() * 1000
-    let waktuwib = require('moment-timezone').tz('Asia/Jakarta').format('HH:mm:ss')
-    let uptime = clockString(_uptime)
-    let d = new Date(new Date + 3600000)
-    let locale = 'id'
-    let week = d.toLocaleDateString(locale, { weekday: 'long' })
-    let date = d.toLocaleDateString(locale, {
-      day: 'numeric',
-      month: 'long',
-      year: 'numeric'
-    })
-    let dateIslamic = Intl.DateTimeFormat(locale + '-TN-u-ca-islamic', {
-      day: 'numeric',
-      month: 'long',
-      year: 'numeric'
-    }).format(d)
-    let _uptime = process.uptime() * 1000
     let time = require('moment-timezone').tz('Asia/Jakarta').format('HH:mm:ss') 
     let runtime = clockString(_uptime)
     let mode = //
