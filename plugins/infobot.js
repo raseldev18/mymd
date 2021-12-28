f33d8c269c48769efe2e4366b627ac87cc1c42fb
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-12-28 14:14:11 +0700

    Update infobot.js

--- plugins/infobot.js
+++ plugins/infobot.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, jid, text }) => {
-await conn.sendTemplateButtonLoc(m.chat, global.infobot.trim(), wm, await(await require('node-fetch')(img)).buffer(), `✨Menu `,`#menu`, m)
- // conn.reply(m.chat, global.infobot, m, 0, { contextInfo: { mentionedJid: conn.parseMention(text) }})
+//await conn.sendTemplateButtonLoc(m.chat, global.infobot.trim(), wm, await(await require('node-fetch')(img)).buffer(), `✨Menu `,`#menu`, m)
+ conn.reply(m.chat, global.infobot, m, 0, { contextInfo: { mentionedJid: conn.parseMention(text) }})
 }
 handler.help = ['infobot']
 handler.tags = ['info']
