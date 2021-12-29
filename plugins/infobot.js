e36c8634cf488f9655b03c5cfde90dc3d769123b
Rasel <90371157+raselcomel@users.noreply.github.com>
2021-12-29 21:21:10 +0700

    Delete infobot.js

--- plugins/infobot.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m, { conn, usedPrefix, text }) => {
-  await conn.sendTemplateButtonLoc(m.chat, global.infobot.trim(), wm, await(await require('node-fetch')(img)).buffer(), `✨Menu `,`#menu`, m)
-}
-handler.help = ['infobot']
-handler.tags = ['info']
-handler.command = /^(info(bot)?)$/i
-
-module.exports = handler
