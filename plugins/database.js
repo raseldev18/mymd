e262c6e3d723aca4952d652b5a51b47bbbef6d2d
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-01-01 05:22:06 +0700

    Hehe

--- plugins/database.js
+++ plugins/database.js
@@ -1,8 +1,8 @@
-let handler = async (m) => {
+let handler = async (m, { text }) => {
     let totalreg = Object.keys(global.db.data.users).length
     let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
     let kon = `*Jumlah database saat ini ${totalreg} user*\n*User registrasi saat ini ${rtotalreg} user*`
-    await sock.sendTemplateButtonLoc(m.chat, kon, wm, await(await require('node-fetch')(fla + 'Database User')).buffer(), 'Menu', '#menu', m)
+    await sock.sendTemplateButtonLoc(m.chat, kon, wm, await(await require('node-fetch')(fla + `${text}`)).buffer(), 'Menu', '#menu', m)
 }
 handler.help = ['user']
 handler.tags = ['info']
