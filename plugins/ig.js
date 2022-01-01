62d5c9b68dac8888f2df34222ab481dcd2db6a36
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-01-02 05:25:59 +0700

    .

--- plugins/ig.js
+++ plugins/ig.js
@@ -17,7 +17,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 }
 handler.help = ['ig'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^(ig|instagram)$/i
+handler.command = /^(ig(dl)?|instagram(dl)?)$/i
 
 handler.limit = true
 
