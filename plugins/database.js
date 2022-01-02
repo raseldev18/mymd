5e37000a534931ede6e9489660ee55a7e88eaa1b
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-01-02 08:35:26 +0700

    >//<

--- plugins/database.js
+++ plugins/database.js
@@ -1,7 +1,7 @@
 let handler = async (m, { usedPrefix, command, conn, text }) => {
     let totalreg = Object.keys(global.db.data.users).length
     let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
-    let kon = `*Jumlah database saat ini ${totalreg} user*\n*User registrasi saat ini ${rtotalreg} user*`
+    let kon = `*Database saat ini ${totalreg} user*\n*Registrasi saat ini ${rtotalreg} user*`
     await conn.sendButtonLoc(m.chat, kon, wm, await(await require('node-fetch')(fla + `${command}`)).buffer(), [['Menu', '#menu']], m)
 }
 handler.help = ['user']
