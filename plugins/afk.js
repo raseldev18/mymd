f86b73bc25dec85c74c365d15a20a659eec92489
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-01-02 09:11:37 +0700

    Oh

--- plugins/afk.js
+++ plugins/afk.js
@@ -1,10 +1,10 @@
-let handler = async(m, { text }) => {
+let handler = async(m, { conn, usedPrefix, text }) => {
     let user = global.db.data.users[m.sender]
     user.afk = +new Date
     user.afkReason = text
-    m.reply(`
+    conn.sendButton(m.chat, `
 ${conn.getName(m.sender)} is now AFK${text ? ': ' + text : ''}
-`)
+`, wm, 0, [[`Menu`, `${usedPrefix}menu`]], m)
 }
 handler.help = ['afk <reason>']
 handler.tags = ['main']
