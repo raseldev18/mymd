a139fd31ddaf92d60ae57a4c3704a01fc06e06b2
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-01-02 11:00:14 +0700

    Hm

--- plugins/ig.js
+++ plugins/ig.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   igdl(args[0]).then(async res => {
     let igdl = JSON.stringify(res)
     let json = JSON.parse(igdl)
-    await m.reply(global.wait)
+    conn.reply(m.chat, global.wait, m)
     for (let { downloadUrl, type } of json) {
       conn.sendFile(m.chat, downloadUrl, 'ig' + (type == 'image' ? '.jpg' : '.mp4'), wm, m)
     }
