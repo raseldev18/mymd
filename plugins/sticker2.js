d7fc739b897713c70da78a76d28179bdf22194b3
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-01-02 12:34:44 +0700

    Hm

--- plugins/sticker2.js
+++ plugins/sticker2.js
@@ -1,4 +1,4 @@
-const { MessageType } = require('@adiwajshing/baileys')
+const { MessageType } = require('@adiwajshing/baileys-md')
 const { sticker } = require('../lib/sticker')
 const WSF = require('wa-sticker-formatter')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
