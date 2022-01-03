ed0c25d265703333bbb01098e30d37f923aea397
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-01-03 23:51:43 +0700

    Create tagall.js

---
+++ plugins/tagall.js
@@ -0,0 +1,19 @@
+let handler = async(m, { isOwner, isAdmin, conn, text, participants }) => {
+  if (!(isAdmin || isOwner)) {
+                global.dfail('admin', m, conn)
+                throw false
+                }
+  let teks = `*───「 TAG ALL 」───*\n${text ? text : ' '}*\n\n`
+		      	for (let mem of participants) {
+		            teks += `@${mem.id.split('@')[0]}\n`
+				}
+                teks += `\n⋙ *Admin Group* ⋘`
+                conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
+}
+handler.help = ['tagall <pesan>']
+handler.tags = ['group']
+handler.command = /^(t(agall)?)$/i
+
+handler.group = true
+
+module.exports = handler
