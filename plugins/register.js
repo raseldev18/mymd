264c9a98719766aa1e22f1b72829ec09f50f6217
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-01-03 13:37:22 +0700

    Create register.js

---
+++ plugins/register.js
@@ -0,0 +1,35 @@
+const { createHash } = require('crypto')
+let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
+let handler = async function (m, { text, usedPrefix, command }) {
+  let user = global.db.data.users[m.sender]
+  if (user.registered === true) throw `Kamu sudah terdaftar\nMau daftar ulang? ${usedPrefix}unreg <SERIAL NUMBER>`
+  if (!Reg.test(text)) throw `contoh:\n*${usedPrefix + command} nama.umur*`
+  let [_, name, splitter, age] = text.match(Reg)
+  if (!name) throw 'Nama tidak boleh kosong (Alphanumeric)'
+  if (!age) throw 'Umur tidak boleh kosong (Angka)'
+  age = parseInt(age)
+  if (age > 40) throw 'Umur terlalu tua'
+  if (age < 5) throw 'Bayi bisa ngetik sesuai format bjir ._., tapi gatau juga si, bocil skrg emg epic². huhu ga kek jamanku'
+  user.name = name.trim()
+  user.age = age
+  user.regTime = + new Date
+  user.registered = true
+  let sn = createHash('md5').update(m.sender).digest('hex')
+  m.reply(`
+Daftar berhasil!
+
+┌─〔 Info 〕
+├ Nama: ${name}
+├ Umur: ${age} tahun
+└────
+
+SN (Serial Number) di kirim di chat pribadi, digunaan untuk daftar ulang
+`.trim())
+conn.sendMessage(m.sender, {text: `SN: ${sn}`})
+}
+handler.help = ['daftar', 'reg', 'register'].map(v => v + ' <nama>.<umur>')
+handler.tags = ['xp']
+
+handler.command = /^(daftar|reg(ister)?)$/i
+
+module.exports = handler
