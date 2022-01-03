e7df41577f55d98718cc1861fa07e64cfd0a5b01
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-01-03 23:06:44 +0700

    Update register.js

--- plugins/register.js
+++ plugins/register.js
@@ -1,35 +1,36 @@
 const { createHash } = require('crypto')
 let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
-let handler = async function (m, { text, usedPrefix, command }) {
+let handler = async function (m, { conn, text, usedPrefix, command }) {
   let user = global.db.data.users[m.sender]
   if (user.registered === true) throw `Kamu sudah terdaftar\nMau daftar ulang? ${usedPrefix}unreg <SERIAL NUMBER>`
-  if (!Reg.test(text)) throw `contoh:\n*${usedPrefix + command} nama.umur*`
+  if (!Reg.test(text)) throw `contoh:\n*${usedPrefix + command} rasel.18*`
   let [_, name, splitter, age] = text.match(Reg)
   if (!name) throw 'Nama tidak boleh kosong (Alphanumeric)'
   if (!age) throw 'Umur tidak boleh kosong (Angka)'
   age = parseInt(age)
-  if (age > 40) throw 'Umur terlalu tua'
-  if (age < 5) throw 'Bayi bisa ngetik sesuai format bjir ._., tapi gatau juga si, bocil skrg emg epic². huhu ga kek jamanku'
+  if (age > 50) throw 'Umur terlalu tua'
+  if (age < 5) throw 'Bayi bisa ngetik sesuai format bjir ._., tapi gatau juga bocil skrg epic² pasti anak ngen ngep:v'
   user.name = name.trim()
   user.age = age
   user.regTime = + new Date
   user.registered = true
   let sn = createHash('md5').update(m.sender).digest('hex')
-  m.reply(`
-Daftar berhasil!
+  conn.reply(m.chat, `
+*Successful Registration*
 
-┌─〔 Info 〕
-├ Nama: ${name}
-├ Umur: ${age} tahun
-└────
+╭─「 Info 」
+│• Nama   : ${name}
+│• Umur   : ${age} tahun
+│• Status : Tedaftar ☑️
+╰─────
 
-SN (Serial Number) di kirim di chat pribadi, digunaan untuk daftar ulang
+SN (Serial Number) di kirim di chat pribadi dan digunaan untuk daftar ulang, jika lupa sn silahkan ketik *${usedPrefix}sn* untuk mengecek sn kamu! 
 `.trim())
 conn.sendMessage(m.sender, {text: `SN: ${sn}`})
 }
 handler.help = ['daftar', 'reg', 'register'].map(v => v + ' <nama>.<umur>')
 handler.tags = ['xp']
 
-handler.command = /^(daftar|reg(ister)?)$/i
+handler.command = /^(daftar|reg(is(ter))?)$/i
 
 module.exports = handler
