4cf8d9e30847ec91c0550c45f5b876741e4cb59d
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-01-03 23:57:12 +0700

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -3,11 +3,11 @@ let handler = async(m, { isOwner, isAdmin, conn, text, participants }) => {
                 global.dfail('admin', m, conn)
                 throw false
                 }
-  let teks = `*───「 TAG ALL 」───*\n${text ? text : ' '}*\n\n`
+  let teks = `*────「 TAG ALL 」────*\n${text ? text : ' '}\n\n`
 		      	for (let mem of participants) {
 		            teks += `@${mem.id.split('@')[0]}\n`
 				}
-                teks += `\n⋙ *Admin Group* ⋘`
+                teks += `\n*${namabot}*`
                 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <pesan>']
