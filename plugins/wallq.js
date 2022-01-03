6b54e67ea72227b951794275ca358ed2d7adbd74
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-01-03 14:13:39 +0700

    Create wallq.js

---
+++ plugins/wallq.js
@@ -0,0 +1,49 @@
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys-md')
+const fetch = require('node-fetch')
+
+let handler = async (m, { usedPrefix, command, conn, text }) => {
+  if (!text) throw 'Nyari apa?'
+  let res = await fetch(global.API('https://wall.alphacoders.com/api2.0', '/get.php', {
+    auth: '3e7756c85df54b78f934a284c11abe4e',
+    method: 'search',
+    term: text
+  }))
+  if (!res.ok) throw eror
+  let json = await res.json()
+  let img = json.wallpapers[Math.floor(Math.random() * json.wallpapers.length)]
+  //await conn.sendFile(m.chat, img.url_image, 'wallpaper', '', m, 0, { thumbnail: Buffer.alloc(0) })
+  let message = await prepareWAMessageMedia({ image: await(await fetch(img.url_image)).buffer()}, { upload: conn.waUploadToServer })
+     const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+      templateMessage: {
+            hydratedTemplate: {
+                imageMessage: message.imageMessage,
+                hydratedContentText: selll,
+                hydratedFooterText: wm,
+                hydratedButtons: [{
+                  index: 0,
+                   urlButton: {
+                        displayText: `📸 Url Wallpaper`,
+                        url: `${img.url_image}`
+                    }
+                }, {
+                   quickReplyButton: {
+                        displayText: `Wallpaper ${text}`,
+                        id: `${usedPrefix}${command} ${text}`
+                    },
+                    selectedIndex: 1
+                }]
+            }
+        }
+    }), { userJid: m.participant || m.key.remoteJid, quoted: m });
+    return await conn.relayMessage(
+        m.key.remoteJid,
+        template.message,
+        { messageId: template.key.id }
+    )
+}
+handler.help = ['wallpaperq <pencarian>']
+handler.tags = ['internet']
+handler.command = /^wall(paper)?q?$/i
+handler.limit = true
+handler.register = false
+module.exports = handler
