b53c75901f954609f0e768836dd8942f1b9f4067
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-01-05 05:01:24 +0700

    Update yta.js

--- plugins/yta.js
+++ plugins/yta.js
@@ -2,17 +2,13 @@ let { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys-md
 let limit = 50
 const { servers, yta } = require('../lib/y2mate')
 let handler = async(m, { conn, args, isPrems, isOwner }) => {
-    if (!args || !args[0]) return m.reply('Uhm... urlnya mana?')
+    if (!args || !args[0]) return conn.reply(m.chat, 'Uhm... urlnya mana?', m)
     let chat = global.db.data.chats[m.chat]
     let server = (args[1] || servers[0]).toLowerCase()
     let { dl_link, thumb, title, filesize, filesizeF } = await yta(args[0], servers.includes(server) ? server : servers[0])
     let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
     conn.reply(m.chat, wait, m)
-    if (!isLimit)
-    await sock.sendMessage(
-    m.chat, 
-    { document: { url: dl_link}, mimetype: 'audio/mpeg', fileName: `${title}.mp3`}
-, {quoted: m})
+    if (!isLimit) await sock.sendMessage(m.chat, { document: { url: dl_link}, mimetype: 'audio/mpeg', fileName: `${title}.mp3`}, {quoted: m})
 }
 handler.help = ['ytmp3 <query>']
 handler.tags = ['downloader']
