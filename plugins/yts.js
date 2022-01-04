c360e8d3dd5ff2a9551e99d558013d3370f790f9
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-01-05 05:29:41 +0700

    Update yts.js

--- plugins/yts.js
+++ plugins/yts.js
@@ -1,26 +1,28 @@
 let yts = require('yt-search')
-let handler = async (m, { text }) => {
-  if (!text) return m.reply('Cari apa?')
+let handler = async (m, { conn, text }) => {
+  if (!text) return conn.eply(m.chat, 'Cari apa?', m)
   let results = await yts(text)
   let teks = results.all.map(v => {
     switch (v.type) {
       case 'video': return `
-*${v.title}* (${v.url})
-Duration: ${v.timestamp}
-Uploaded ${v.ago}
-${v.views} views
-      `.trim()
+*Judul:* ${v.title} 
+*Link:* (${v.url})
+*Duration:* ${v.timestamp}
+*Uploaded:* ${v.ago}
+*Viewer:* ${v.views} 
+ `.trim()
       case 'channel': return `
-*${v.name}* (${v.url})
-_${v.subCountLabel} (${v.subCount}) Subscriber_
-${v.videoCount} video
+*Chanel:* ${v.name} 
+*Link:* (${v.url})
+*Subscriber:* ${v.subCountLabel} (${v.subCount})
+*Total Video:* ${v.videoCount} video
 `.trim()
     }
-  }).filter(v => v).join('\n========================\n')
-  m.reply(teks)
+  }).filter(v => v).join('\n*=========================*\n')
+ conn.reply(m.chat, teks, m)
 }
 handler.help = ['ytsearch <query>']
-handler.tags = ['tools']
+handler.tags = ['tools', 'internet']
 handler.command = /^yts(earch)?$/i
 
 module.exports = handler
