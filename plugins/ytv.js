4482cb28ed62791b71264993c98dd0599665955f
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-01-05 04:58:35 +0700

    Update ytv.js

--- plugins/ytv.js
+++ plugins/ytv.js
@@ -2,7 +2,7 @@ let limit = 50
 let fetch = require('node-fetch')
 const { servers, ytv } = require('../lib/y2mate')
 let handler = async(m, { conn, args, isPrems, isOwner }) => {
-    if (!args || !args[0]) return m.reply('Uhm... urlnya mana?')
+    if (!args || !args[0]) return conn.reply(m.chat, 'Uhm... urlnya mana?', m)
     let chat = global.db.data.chats[m.chat]
     let server = (args[1] || servers[0]).toLowerCase()
     let { dl_link, thumb, title, filesize, filesizeF } = await ytv(args[0], servers.includes(server) ? server : servers[0])
