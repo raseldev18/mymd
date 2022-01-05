7b30aa4a631b85e6895876710566f1166361d34b
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-01-06 05:06:07 +0700

    Update speed.js

--- plugins/speed.js
+++ plugins/speed.js
@@ -1,24 +1,87 @@
+
 let { performance } = require('perf_hooks')
-let { MessageType } = require('@adiwajshing/baileys-md')
-let fetch = require('node-fetch')
+let osu = require('node-os-utils')
 let handler = async(m, { conn, command, usedPrefix, DevMode }) => {
     try {
+        let NotDetect = 'Not Detect'
         let old = performance.now()
-        await conn.reply(m.chat, '_Testing speed..._', m)
+        let cpu = osu.cpu
+        let cpuCore = cpu.count()
+        let drive = osu.drive
+        let mem = osu.mem
+        let netstat = osu.netstat
+        let OS = osu.os.platform()
+        let cpuModel = cpu.model()
+        let cpuPer
+        let p1 = cpu.usage().then(cpuPercentage => {
+            cpuPer = cpuPercentage
+        }).catch(() => {
+            cpuPer = NotDetect
+        })
+        let driveTotal, driveUsed, drivePer
+        let p2 = drive.info().then(info => {
+            driveTotal = (info.totalGb + ' GB'),
+                driveUsed = info.usedGb,
+                drivePer = (info.usedPercentage + '%')
+        }).catch(() => {
+            driveTotal = NotDetect,
+                driveUsed = NotDetect,
+                drivePer = NotDetect
+        })
+        let ramTotal, ramUsed
+        let p3 = mem.info().then(info => {
+            ramTotal = info.totalMemMb,
+                ramUsed = info.usedMemMb
+        }).catch(() => {
+            ramTotal = NotDetect,
+                ramUsed = NotDetect
+        })
+        let netsIn, netsOut
+        let p4 = netstat.inOut().then(info => {
+            netsIn = (info.total.inputMb + ' MB'),
+                netsOut = (info.total.outputMb + ' MB')
+        }).catch(() => {
+            netsIn = NotDetect,
+                netsOut = NotDetect
+        })
+        await Promise.all([p1, p2, p3, p4])
+        await conn.reply(m.chat, `_Testing ${command }_`, m)
+        let _ramTotal = (ramTotal + ' MB')
         let neww = performance.now()
-        conn.sendButtonLoc(m.chat, neww - old + ' milidetik 💬', wm, await(await fetch(fla + `${command}`)).buffer(), [[`Menu`, `${usedPrefix}menu`]], m)
+        conn.sendButtonLoc(m.chat, `
+*───「 Info Testing 」───*
+
+OS : *${OS}*
+CPU Model : *${cpuModel}*
+CPU Core : *${cpuCore} Core*
+CPU : *${cpuPer}%*
+Ram : *${ramUsed} / ${_ramTotal}(${/[0-9.+/]/g.test(ramUsed) &&  /[0-9.+/]/g.test(ramTotal) ? Math.round(100 * (ramUsed / ramTotal)) + '%' : NotDetect})*
+Drive : *${driveUsed} / ${driveTotal} (${drivePer})*
+Ping : *${Math.round(neww - old)} ms*
+Internet IN : *${netsIn}*
+Internet OUT : *${netsOut}*
+`.trim(), wm, await(await require('node-fetch')(fla + `${command }`)).buffer(), [[`Menu`, `${usedPrefix}menu`]], m)
+        console.log(OS)
     } catch (e) {
         console.log(e)
-        m.reply('Error!!')
+        conn.reply(m.chat, eror, m)
         if (DevMode) {
             for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)) {
-                conn.sendMessage(jid, 'Speed.js error\nNo: *' + m.sender.split `@` [0] + '*\nCommand: *' + m.text + '*\n\n*' + e + '*', MessageType.text)
+                conn.reply(jid, 'Status.js error\nNo: *' + m.sender.split `@` [0] + '*\nCommand: *' + m.text + '*\n\n*' + e + '*', m)
             }
         }
     }
 }
 handler.help = ['speed']
 handler.tags = ['info']
+handler.command = /^(speed|ping|(bot)?stat(us)?(bot)?)$/i
 
-handler.command = /^(ping|speed)$/i
 module.exports = handler
+
+function clockString(ms) {
+    let h = Math.floor(ms / 3600000)
+    let m = Math.floor(ms / 60000) % 60
+    let s = Math.floor(ms / 1000) % 60
+    console.log({ ms, h, m, s })
+    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+}
