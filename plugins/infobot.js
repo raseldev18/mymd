54dd5da577bef91b426ee2e6ea06049282484ea4
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-01-05 14:17:08 +0700

    Update infobot.js

--- plugins/infobot.js
+++ plugins/infobot.js
@@ -39,3 +39,11 @@ handler.tags = ['info']
 handler.command = /^(info(bot)?)$/i
 
 module.exports = handler
+
+function clockString(ms) {
+    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+}
+
