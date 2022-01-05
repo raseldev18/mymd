76d4d4a3b7dd16dbce81d72579d749d10a0e2380
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-01-05 23:49:29 +0700

    Jngn sange di gc banh

--- plugins/nsfwloli.js
+++ plugins/nsfwloli.js
@@ -1,6 +1,8 @@
 const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys-md')
 let fetch = require('node-fetch')
    let handler  = async (m, { conn, usedPrefix, command }) => {
+   var {age} = db.data.users[m.sender]
+   if (age <17) throw conn.reply(m.chat, 'Lu masih di bawah umur jangan dulu deh', m)
    let lolinya = `https://api.lolhuman.xyz/api/random/nsfw/loli?apikey=Dawnfrostkey` 
    //conn.sendButtonImg(m.chat, json, 'Jangan coli ya', wm, '➡️ NEXT', `${usedPrefix + command}`, m, false)
    let message = await prepareWAMessageMedia({ image: await(await fetch(lolinya)).buffer()}, { upload: conn.waUploadToServer })
