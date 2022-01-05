340900eb57c3548374db5121859906dc116fe775
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-01-05 14:54:16 +0700

    Update infobot.js

--- plugins/infobot.js
+++ plugins/infobot.js
@@ -1,38 +1,36 @@
-const simple = require('./lib/simple')
-const util = require('util')
-
-const isNumber = x => typeof x === 'number' && !isNaN(x)
-const delay = ms => isNumber(ms) && new Promise(resolve => setTimeout(resolve, ms))
-
 let handler = async (m, { conn, command, usedPrefix, text }) => {
+  let fetch = require('node-fetch')
+  let _uptime = process.uptime() * 1000
+  let a = require('moment-timezone').tz('Asia/Jakarta').format('HH:mm:ss') 
   let d = new Date(new Date + 3600000)
-    let locale = 'id'
+  let locale = 'id'
     let week = d.toLocaleDateString(locale, { weekday: 'long' })
     let date = d.toLocaleDateString(locale, {
       day: 'numeric',
       month: 'long',
       year: 'numeric'
     })
-  let _uptime = process.uptime() * 1000
-  let time = require('moment-timezone').tz('Asia/Jakarta').format('HH:mm:ss') 
   let runtime = clockString(_uptime)
   let usergakdaftar = Object.keys(global.db.data.users).length
   let userdaftar = Object.values(global.db.data.users).filter(user => user.registered == true).length
   let infonyacok = `
-╭──「 ${namabot} ──࿐
+╭──「 ${conn.user.name} ──࿐
 │✇ Library : *Baileys-MD*
 │✇ Language : *Javascript*
 │✇ Database : *MongoDB*
 │✇ Version : *^0.0.1*
 │✇ Dev : *@6285346545126*
 │✇ Runtime : *${runtime}*
-│✇ Prefix : *Multi Prefix 「 ${usedPrefix} 」*
+│✇ Prefix : *Multi Prefix 「 ${usedPrefix}」*
 │✇ Mode : *${global.opts['self'] ? 'Self' : 'Public'}*
 │✇ User : *${usergakdaftar}*
 │✇ Register : *${userdaftar}*
 ╰─────────⳹
 `.trim()
-  await conn.sendButtonLoc(m.chat, infonyacok, `${week}, ${date}\n${time}`, await(await require('node-fetch')(fla + `${command}`)).buffer(), [[`Menu `,`${usedPrefix}menu`]], m)
+var as = `Tanggal : ${week}, ${date}\nWaktu : ${a} (WIB)`
+
+  await conn.sendButtonLoc(m.chat, infonyacok, as, await(await fetch(fla + `${command}`)).buffer(), [[`Menu`,  `${usedPrefix}menu`]], m)
+
 }
 handler.help = ['infobot']
 handler.tags = ['info']
