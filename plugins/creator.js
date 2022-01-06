a1e81c6bf2b7eb82ec47e074cbd22b72a2e36dbe
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-01-06 08:05:10 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 function handler(m) {
   this.sendContact(m.chat, global.owner[0] + '@s.whatsapp.net', this.getName(global.owner[0] + '@s.whatsapp.net'), m)
-  return conn.reply(m.chat, `Halo kak @${m.sender.split`@`[0]} itu nomor ownerku jangan di apa-apain ya kak😖`, m)
+  return conn.reply(m.chat, `Halo kak @${await this.getName(m.sender)} itu nomor ownerku jangan di apa-apain ya kak😖`, m)
 }
 handler.help = ['owner']
 handler.tags = ['info']
