e43b804c7972b57551313995f13873295e4ed45a
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-01-06 23:14:01 +0700

    Update linkgroup.js

--- plugins/linkgroup.js
+++ plugins/linkgroup.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
-    conn.reply(m.chat, `*Link Group:* ` + this.getName(m.chat) + `\n\nhttps://chat.whatsapp.com/` + await conn.groupInviteCode(m.chat), m)
+    conn.reply(m.chat, `*Link Group:* ` + conn.getName(m.chat) + `\n\nhttps://chat.whatsapp.com/` + await conn.groupInviteCode(m.chat), m)
   }
   handler.help = ['linkgroup']
   handler.tags = ['group']
