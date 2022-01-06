a6ecc6308640209e8440d2816079c8ecd9ea7ff6
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-01-06 09:42:45 +0700

    Create revoke.js

---
+++ plugins/revoke.js
@@ -0,0 +1,17 @@
+let handler = async (m, { isAdmin, isOwner, conn }) => {
+  if (!(isAdmin || isOwner)) {
+                global.dfail('admin', m, conn)
+                throw false
+                }
+  let res = await conn.revokeInvite(m.chat)
+  conn.reply(m.chat, `Sukses ${command} link grup, link telah di kirim lewat chat pribadi`, m) 
+  conn.reply(m.sender, 'https://chat.whatsapp.com/' + res.code, m)
+}
+handler.help = ['revoke']
+handler.tags = ['group']
+handler.command = /^re(voke|new|set)(invite|link)?$/i
+
+handler.group = true
+handler.botAdmin = true
+
+module.exports = handler
