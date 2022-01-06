eb618c95735b2055aeb39ae16ab8e9f16ffd40ee
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-01-06 09:46:25 +0700

    Update revoke.js

--- plugins/revoke.js
+++ plugins/revoke.js
@@ -3,7 +3,7 @@ let handler = async (m, { isAdmin, isOwner, conn }) => {
                 global.dfail('admin', m, conn)
                 throw false
                 }
-  let res = await conn.revokeInvite(m.chat)
+  let res = await conn.groupInviteCode(m.chat)
   conn.reply(m.chat, `Sukses ${command} link grup, link telah di kirim lewat chat pribadi`, m) 
   conn.reply(m.sender, 'https://chat.whatsapp.com/' + res.code, m)
 }
