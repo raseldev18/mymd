0b638c9f16f4568fa602c9723e4ee0cf3700cff9
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-01-07 16:00:39 +0700

    Biar fast dikit

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 const delay = time => new Promise(res => setTimeout(res, time))
 let handler = async(m, { conn }) => {
   conn.sendContact(m.chat, global.owner[0] + '@s.whatsapp.net', conn.getName(global.owner[0] + '@s.whatsapp.net'), m)
-  await delay(1000)
+  await delay(500)
   conn.reply(m.chat, `Halo kak ${await conn.getName(m.sender)} itu nomor ownerku jangan di apa-apain ya kak😖`, m)
 }
 handler.help = ['owner']
