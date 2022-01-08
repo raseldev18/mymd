014cae386246196d27ceb194dc56a7696f538408
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-01-08 00:16:59 +0000

    Tes

---
+++ plugins/caklontong.js
@@ -0,0 +1,33 @@
+let fetch = require('node-fetch')
+let timeout = 120000
+let poin = 500
+let handler = async (m, { conn, command, usedPrefix }) => {
+    conn.caklontong = conn.caklontong ? conn.caklontong : {}
+    let id = m.chat
+    if (id in conn.caklontong) {
+        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.caklontong[id][0])
+        throw false
+    }
+    let src = await (await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/caklontong.json')).json()
+    let json = src[Math.floor(Math.random() * src.length)]
+    let caption = `
+${json.soal}
+
+Timeout *${(timeout / 1000).toFixed(2)} detik*
+Ketik ${usedPrefix}calo untuk bantuan
+Bonus: ${poin} XP
+`.trim()
+    conn.caklontong[id] = [
+        await conn.sendButtonLoc(m.chat, caption, wm, await(await fetch(fla + `${command}`)).buffer(),, [['Bantuan', '.calo']], m),
+        json, poin,
+        setTimeout(async () => {
+            if (conn.caklontong[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*\n${json.deskripsi}`, wm, false, [['Cak Lontong', '.caklontong']], conn.caklontong[id])
+            delete conn.caklontong[id]
+        }, timeout)
+    ]
+}
+handler.help = ['caklontong']
+handler.tags = ['game']
+handler.command = /^(caklon(tong)?)$/i
+
+module.exports = handler
