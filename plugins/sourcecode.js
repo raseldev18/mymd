7aa3355c820afb95b86e3614fff73975581b8aa7
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-03-29 21:49:40 +0700

    Update sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -1,5 +1,20 @@
 let handler = async (m, {conn}) => {
-     conn.reply(m.chat, `Bot ini menggunakan script github\n\nhttps://github.com/raselcomel/lucubot-md\n\ndon't forget to give star ✨`, m) 
+  let pp = let pp = await conn.profilePictureUrl(who, 'image').catch((_) => "https://telegra.ph/file/24fa902ead26340f3df2c.png")
+  conn.reply(m.chat, `Bot ini menggunakan script github
+     
+https://github.com/raselcomel/mymd
+     
+don't forget to give star ✨`, m, {
+  contextInfo: { mentionedJid: [m.sender],
+    externalAdReply :{
+    mediaUrl: web,
+    mediaType: 2,
+    description: 'des', 
+    title: `Hello ${m.name}`,
+    body: wm, 
+    thumbnail: await(await fetch(pp)).buffer()
+   }}           
+}) 
 }
 handler.help = ['sourcecode']
 handler.tags = ['info']
