2bc0be4abf45958e5706e2554a92e434618315a6
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-03-29 17:02:28 +0700

    anu

--- plugins/speed.js
+++ plugins/speed.js
@@ -139,7 +139,7 @@ ${readMore}
 • *${groups.length - groupsIn}* Groups Left
 • *${chats.length - groups.length}* Personal Chats
 • *${chats.length}* Total Chats
-• *${conn.fetchBlocklist().length}* Terblock
+• *${await conn.fetchBlocklist().length}* Terblock
 • *${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length}* Chat Terbanned
 • *${Object.entries(global.db.data.users).filter(user => user[1].banned).length}* Pengguna Terbanned
 
@@ -173,7 +173,6 @@ Ping: *${speed} ms*
 Internet IN: *${netsIn}*
 Internet OUT: *${netsOut}*
 Runtime : *${uptime} / (${muptime})*
-Time Zone: *${timez}*
 Time Server: *${times}*
 
 _NodeJS Memory Usage_
