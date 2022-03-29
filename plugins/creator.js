0bcfad7e89dcf3b41e1ad045d5fbade6c936352f
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-03-29 21:02:15 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,4 +1,5 @@
 let handler = async(m, { conn }) => {
+  let name = m.sender
   let fkonn = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: '6285346545126@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${await conn.getName(name)}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
   try {
   const sentMsg = await conn.sendContactArray(m.chat, [
