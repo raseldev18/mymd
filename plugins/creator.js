316b4d628bb6472303807552446b9c95f322c3f1
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-03-29 17:11:53 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -2,7 +2,7 @@ let handler = async(m, { conn }) => {
   try {
   const sentMsg = await conn.sendContactArray(m.chat, [
     [`${owner[0]}`, `${await conn.getName(owner[0]+'@s.whatsapp.net')}`, `👑 Developer Bot `, `🚫 Don't call me 😡`, `raselcomel18@gmail.com`, `🇮🇩 Indonesia`, `🛸 raselcomel.github.io`, `👤 Gada pawang nih senggol dong 😣`],
-    [`${owner[1]}`, `${await conn.getName(owner[1]+'@s.whatsapp.net')}`, `Ayangnya rasel 😋`, `📵 Don't spam/call me 😡`, ``, ``, `🚀 https://instagram.com/rasel.ganz`, `🤖 Hanya bot biasa yang kadang suka eror ☺`]
+    [`${owner[1]}`, `${await conn.getName(owner[1]+'@s.whatsapp.net')}`, `Tukang banned 🗿`, `Kasi piltek om:v`, `mark@`, `Idk`, `Idk`, `Empat sehat le mark sempurnaツ`]
     //[`${global.owner[2]}`, `Amirul`, ``, ``, ``, `🇮🇩 Indonesia`, ``, ``]
   ], m) 
   await conn.reply(m.chat, `Halo kak @${m.sender.split(`@`)[0]} itu nomor ownerku jangan di apa-apain ya kak😖`, sentMsg, {mentions: [m.sender]})
