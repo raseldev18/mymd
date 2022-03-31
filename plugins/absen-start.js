6bbd91f99cd388d1ef84aa514adf302590b4041f
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-03-31 14:09:14 +0700

    Update absen-start.js

--- plugins/absen-start.js
+++ plugins/absen-start.js
@@ -6,26 +6,23 @@ let handler = async (m, { conn, isAdmin, isOwner, usedPrefix, text, participants
     conn.absen = conn.absen ? conn.absen : {}
     let id = m.chat
     let lang = db.data.users[m.sender].language
-    if (!(id in conn.absen)) {
-        let ca = `Tidak ada absen berlangsung digrup ini!` 
-        let ca2 = `ketik ${usedPrefix}mulaiabsen untuk memulai absen`
-        let capt = await conn.trans(lang, ca).catch(async _ => await conn.trans2(lang, ca)) 
-        let capt2 = await conn.trans(lang, ca2).catch(async _ => await conn.trans2(lang, ca2)) 
-        let captionn = '_*'+capt+'*_\n\n'+capt2
-        conn.sendB(m.chat, captionn, wm, null, [[await conn.trans(lang, 'Mulai Absen').catch(async _ => await conn.trans2(lang, 'Mulai Absen')), `${usedPrefix}mulaiabsen`]], m)
+    if (id in conn.absen) {
+        let ms = `Masih ada absen di chat ini!\n\nketik ${usedPrefix}hapusabsen untuk menghapus absen`
+        let msh = await conn.trans(lang, ms).catch(async _ => await conn.trans2(lang, ms))
+        conn.sendB(m.chat, msh, wm, null, [[await conn.trans(lang, 'Hapus Absen').catch(async _ => await conn.trans2(lang, 'Hapus Absen')), `${usedPrefix}hapusabsen`]], m)
         throw false
     }
     let hid = await conn.groupMetadata(m.chat)
     let amu = await conn.trans(lang, 'Absen dimulai').catch(async _ => await conn.trans2(lang, 'Absen dimulai'))
     conn.absen[id] = [   
-        conn.sendB(m.chat, amu, wm, null, [[await conn.trans(lang, 'Hadir').catch(async _ => await conn.trans2(lang, 'Hadir')), `${usedPrefix}startabsen`]], m, {mentions: hid.participants.map(a => a.id)}),
+        conn.sendB(m.chat, amu, wm, null, [[await conn.trans(lang, 'Absen').catch(async _ => await conn.trans2(lang, 'Absen')), `${usedPrefix}absen`]], m, {mentions: hid.participants.map(a => a.id)}),
         [],
         text
     ]
 }
-handler.help = ['startabsent <text>']
+handler.help = ['mulaiabsen [teks]']
 handler.tags = ['absen']
-handler.command = /^((start|mulai)absent?|absent?(start?|mulai))$/i
+handler.command = /^(start|mulai)absen|absen(start|mulai)$/i
 
 handler.group = true
 
