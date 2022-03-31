c5c53724b8f9e26605b83e5e0f65d6de439e4e9a
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-04-01 05:37:15 +0700

    Update addmods.js

--- plugins/addmods.js
+++ plugins/addmods.js
@@ -2,11 +2,11 @@ let fs = require('fs')
 let handler = async (m, { conn, args }) => {
     let lang = db.data.users[m.sender].language
     const json = JSON.parse(fs.readFileSync('./src/moderator.json'))
+    try {
     let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : args[0].replace(/[^0-9]/g, '') + '@s.whatsapp.net'
     else who = args[0] ? args[0].replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat
-    if (!who) return m.reply(await conn.trans(lang, '@tag atau balas pesan user').catch(async _ => await conn.trans2(lang, '@tag atau balas pesan user')))
-    if (!args) return m.reply(let capt = await conn.trans(lang, 'Angkanya mana?').catch(async _ => await conn.trans2(lang, 'Angkanya mana?')))
+    if (!args) return m.reply(await conn.trans(lang, 'Angkanya mana?').catch(async _ => await conn.trans2(lang, 'Angkanya mana?')))
     if (isNaN(args)) return await conn.trans(lang, 'Hanya angka, mewakili hari!').catch(async _ => await conn.trans2(lang, 'Hanya angka, mewakili hari!'))
     let ohh = `${await conn.getName(who)} sudah jadi moderator!`
     let sdh = await conn.trans(lang, ohh).catch(async _ => await conn.trans2(lang, ohh))
@@ -21,6 +21,10 @@ let handler = async (m, { conn, args }) => {
     let user = db.data.users[m.sender]
     user.moderator = true
     user.moderatorTime = args
+    } catch (e) {
+      return m.reply(await conn.trans(lang, '@tag atau balas pesan user').catch(async _ => await conn.trans2(lang, '@tag atau balas pesan user')))
+      console.log(e)
+  }
 }
 handler.help = ['addmods [@user]']
 handler.tags = ['owner']
