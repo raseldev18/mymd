63be7e47eb95150dfdac1b88ce74c6854fe0168f
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-03-31 20:22:19 +0700

    ✨ update ✨

--- plugins/absen-start.js
+++ plugins/absen-start.js
@@ -7,22 +7,25 @@ let handler = async (m, { conn, isAdmin, isOwner, usedPrefix, text, participants
     let id = m.chat
     let lang = db.data.users[m.sender].language
     if (id in conn.absen) {
-        let ms = `Masih ada absen di chat ini!\n\nketik ${usedPrefix}hapusabsen untuk menghapus absen`
-        let msh = await conn.trans(lang, ms).catch(async _ => await conn.trans2(lang, ms))
-        conn.sendB(m.chat, msh, wm, null, [[await conn.trans(lang, 'Hapus Absen').catch(async _ => await conn.trans2(lang, 'Hapus Absen')), `${usedPrefix}hapusabsen`]], m)
+        let ca = `Masih ada absen di digrup ini`
+        let cb = `Ketik ${usedPrefix}hapusabsen untuk menghapus absen!`
+        let cc = await conn.trans(lang, ca).catch(async _ => await conn.trans2(lang, ca))
+        let cd = await conn.trans(lang, cb).catch(async _ => await conn.trans2(lang, cb)) 
+        let capt = '_*'+cc+'*_\n\n'+cd
+        conn.sendB(m.chat, capt, wm, null, [[await conn.trans(lang, 'Hapus Absen').catch(async _ => await conn.trans2(lang, 'Hapus Absen')), `${usedPrefix}mulaiabsen`]], m)
         throw false
     }
     let hid = await conn.groupMetadata(m.chat)
     let amu = await conn.trans(lang, 'Absen dimulai').catch(async _ => await conn.trans2(lang, 'Absen dimulai'))
     conn.absen[id] = [   
-        conn.sendB(m.chat, amu, wm, null, [[await conn.trans(lang, 'Absen').catch(async _ => await conn.trans2(lang, 'Absen')), `${usedPrefix}absen`]], m, {mentions: hid.participants.map(a => a.id)}),
+        conn.sendB(m.chat, amu, wm, null, [[await conn.trans(lang, 'Hadir').catch(async _ => await conn.trans2(lang, 'Hadir')), `${usedPrefix}absen`]], m, {mentions: hid.participants.map(a => a.id)}),
         [],
         text
     ]
 }
-handler.help = ['mulaiabsen [teks]']
+handler.help = ['startabsen <text>']
 handler.tags = ['absen']
-handler.command = /^(start|mulai)absen|absen(start|mulai)$/i
+handler.command = /^(start|mulai)absent?|absent?(start|mulai)$/i
 
 handler.group = true
 
