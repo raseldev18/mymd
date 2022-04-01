cef9e4d23b78abf22514dd52c25fbe74b916efa0
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-04-01 21:36:27 +0700

    Update addmods.js

--- plugins/addmods.js
+++ plugins/addmods.js
@@ -1,21 +1,18 @@
 let fs = require('fs')
 let handler = async (m, { conn, args }) => {
-    try {
+
     const json = JSON.parse(fs.readFileSync('./src/moderator.json'))
     let who
-    if (m.isGroup) m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender ? m.quoted.sender : args[0].replace(/[^0-9]/g, '') + '@s.whatsapp.net'
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : args[0].replace(/[^0-9]/g, '') + '@s.whatsapp.net'
     else who = args[0] ? args[0].replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat
-    if (!who) throw '@tag atau balas!'
-    if (json.includes(who.split`@`[0])) throw `${await conn.getName(who)} sudah moderator!`
+    if (json.includes(who.split`@`[0])) throw `${await conn.getName(who)} sudah owner!`
     json.push(`${who.split`@`[0]}`)
     fs.writeFileSync('./src/moderator.json', JSON.stringify(json))
-    m.reply2(`${await conn.getName(who)} sekarang adalah moderator!`)
+    m.reply2(`${await conn.getName(who)} sekarang adalah owner!`)
+
     delete require.cache[require.resolve('../config')]
     require('../config')
-    } catch(e) {
-       throw `@tag atau balas!`
-       console.log(e)
-       }
+
 }
 handler.help = ['addmods [@user]']
 handler.tags = ['owner']
