7473c06830df911287d7fc4e7d7702f51e071c8a
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-04-01 16:05:30 +0700

    ✨ fix ✨

--- plugins/addmods.js
+++ plugins/addmods.js
@@ -3,9 +3,9 @@ let handler = async (m, { conn, args }) => {
 
     const json = JSON.parse(fs.readFileSync('./src/moderator.json'))
     let who
-    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted// ? m.quoted.sender : args[0].replace(/[^0-9]/g, '') + '@s.whatsapp.net'
-    else who = m.quoted//args[0] ? args[0].replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat
-    if(!who) throw `@tag atau reply`
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender// ? m.quoted.sender : args[0].replace(/[^0-9]/g, '') + '@s.whatsapp.net'
+    else who = m.quoted.sender//args[0] ? args[0].replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat
+    if(!who) throw `@tag atau balas!`
     if (json.includes(who.split`@`[0])) throw `${await conn.getName(who)} sudah jadi moderator!`
     json.push(`${who.split`@`[0]}`)
     fs.writeFileSync('./src/moderator.json', JSON.stringify(json))
@@ -17,7 +17,7 @@ let handler = async (m, { conn, args }) => {
 }
 handler.help = ['addmods [@user]']
 handler.tags = ['owner']
-handler.command = /^(add|tambah|\+)mo(ds)?$/i
+handler.command = /^(add|tambah|\+)mod?s?$/i
 
 handler.rowner = true
 
