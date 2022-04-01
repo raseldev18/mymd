668a0718b199a24e4afdc21e2ca6a6683c1ed95d
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-04-01 21:16:05 +0700

    Update addmods.js

--- plugins/addmods.js
+++ plugins/addmods.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, args }) => {
     const json = JSON.parse(fs.readFileSync('./src/moderator.json'))
     let who
     if (m.isGroup) m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender ? m.quoted.sender : args[0].replace(/[^0-9]/g, '') + '@s.whatsapp.net'
-    else who = args[0].replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat
+    else who = args[0] ? args[0].replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat
     if (!who) throw '@tag atau balas!'
     if (json.includes(who.split`@`[0])) throw `${await conn.getName(who)} sudah moderator!`
     json.push(`${who.split`@`[0]}`)
