56ac215e0d33f86a195ab0a9532ea6152c0f565c
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-04-02 17:37:51 +0700

    Update _premium.js

--- plugins/_premium.js
+++ plugins/_premium.js
@@ -6,7 +6,7 @@ handler.before = async function (m) {
     if (m.chat.endsWith('broadcast')) return
     if (user.premiumTime != 0 && user.premium) {
         if (new Date() * 1 >= user.premiumTime) {
-            this.reply(m.sender, teks, null, {mentions: this.parseMention(teks)})
+            this.reply2(m.sender, teks, null, {mentions: this.parseMention(teks)})
             const json = JSON.parse(fs.readFileSync('./src/premium.json'))
             let index = json.findIndex(v => (v.replace(/[^0-9]/g, '') + '@s.whatsapp.net') === (who.replace(/[^0-9]/g, '') + '@s.whatsapp.net'))
             if (json.includes(who)) throw false 
