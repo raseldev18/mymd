9e02ea873bacce586b9d5b699ed7f7d36a4f91e9
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-04-02 17:36:37 +0700

    Update and rename _moderator.js to _premium.js

--- plugins/_premium.js
+++ plugins/_premium.js
@@ -2,18 +2,18 @@ let handler = m => m
 
 handler.before = async function (m) {
     let user = db.data.users[m.sender]
-    let teks = `Hai ${m.name} waktu moderator kamu sudah habis, jika ingin berlangganan moderator lagi silahkan hubungi: @${owner[0]+'@s.whatsapp.net'} terimakasih:)`
+    let teks = `Hai ${m.name} waktu premium kamu sudah habis, jika ingin berlangganan lagi silahkan hubungi: @${owner[0]+'@s.whatsapp.net'} terimakasih:)`
     if (m.chat.endsWith('broadcast')) return
-    if (user.moderatorTime != 0 && user.moderator) {
-        if (new Date() * 1 >= user.moderatorTime) {
+    if (user.premiumTime != 0 && user.premium) {
+        if (new Date() * 1 >= user.premiumTime) {
             this.reply2(m.sender, teks, null, {mentions: this.parseMention(teks)})
-            const json = JSON.parse(fs.readFileSync('./src/moderator.json'))
+            const json = JSON.parse(fs.readFileSync('./src/premium.json'))
             let index = json.findIndex(v => (v.replace(/[^0-9]/g, '') + '@s.whatsapp.net') === (who.replace(/[^0-9]/g, '') + '@s.whatsapp.net'))
             if (json.includes(who)) throw false 
             json.splice(index, 1)
-            fs.writeFileSync('./src/moderator.json', JSON.stringify(json))
-            user.moderatorTime = 0
-            user.moderator = false
+            fs.writeFileSync('./src/premium.json', JSON.stringify(json))
+            user.premiumTime = 0
+            user.premium = false
         }
     }
 }
