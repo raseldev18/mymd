562219315754fbec5da5502ed1499cc210293f59
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-04-02 14:07:16 +0700

    Update stickerOk.js

--- plugins/stickerOk.js
+++ plugins/stickerOk.js
@@ -2,7 +2,7 @@ let handler = async(m, { conn }) => {
   let a = "https://telegra.ph/file/90d392a787612bd894610.png"
   conn.sendStimg(m.chat, a, m, {packname: packname, author: author })
 }
-handler.customPrefix= /^terima|makasih|mksh|thank|thanks$/i
+handler.customPrefix= /^makasih|kasih|mksh|thank|thanks$/i
 handler.command = new RegExp
 
 module.exports = handler
