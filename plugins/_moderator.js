26c3476f7d3b31439898838583018b08aca2a052
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-04-02 09:49:47 +0700

    ✨ fix ✨

--- plugins/_moderator.js
+++ plugins/_moderator.js
@@ -1,13 +1,12 @@
 let handler = m => m
 
 handler.before = async function (m) {
-	let who = m.sender
     let user = db.data.users[m.sender]
     let teks = `Hai ${m.name} waktu moderator kamu sudah habis, jika ingin berlangganan moderator lagi silahkan hubungi: @${owner[0]+'@s.whatsapp.net'} terimakasih:)`
     if (m.chat.endsWith('broadcast')) return
     if (user.moderatorTime != 0 && user.moderator) {
         if (new Date() * 1 >= user.moderatorTime) {
-            this.reply(who, teks, null, {mentions: this.parseMention(teks})
+            this.reply2(m.sender, teks, null, {mentions: this.parseMention(teks)})
             const json = JSON.parse(fs.readFileSync('./src/moderator.json'))
             let index = json.findIndex(v => (v.replace(/[^0-9]/g, '') + '@s.whatsapp.net') === (who.replace(/[^0-9]/g, '') + '@s.whatsapp.net'))
             if (json.includes(who)) throw false 
