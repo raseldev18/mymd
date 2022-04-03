0335cac1b9f703748dc061fa4a039ae6403e2221
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-04-03 18:37:32 +0700

    For vps installation

---
+++ vps.sh
@@ -0,0 +1,8 @@
+sudo apt udate && upgrade
+sudo apt install curl
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+source ~/.profile
+nvm install nodejs
+sudo apt install ffmpeg
+sudo apt install ffmpeg
+nvm ls-remote
