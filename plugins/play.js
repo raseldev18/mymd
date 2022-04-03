168d187dd8266c3d36fc0a1357ee92a52af60b9f
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-04-03 16:42:30 +0700

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -1,20 +1,14 @@
 const { youtubeSearch } = require('@bochilteam/scraper')
 let handler = async (m, { conn, command, text, usedPrefix }) => {
   let lang = db.data.users[m.sender].language
-    let con = `Contoh: ${usedPrefix}${command} i see your monster`
-    let teks = await conn.trans(lang, con).catch(async _ => await conn.trans2(lang, con))
-    if(!text) return m.reply(teks)
-    let w = await conn.trans(lang, wait).catch(async _ => await conn.trans2(lang, wait))
-    m.reply(w)
-    let titlex = await conn.trans(lang, titlink).catch(async _ => await conn.trans2(lang, titlink))
-    let ucap = await conn.trans(lang, 'Selamat menonton').catch(async _ => await conn.trans2(lang, 'Selamat menonton'))
+    if(!text) throw `Contoh: ${usedPrefix}${command} i see your monster`
+    m.reply2(wait)
     let anu = await youtubeSearch(text)
     let vid = anu.video
     let vide 
     if (/playrand(om)?$/i.test(command)) vide = conn.rand(vid)
     else vide = vid[0]
-    let novid = await conn.trans(lang, 'Video/Audio Tidak ditemukan').catch(async _ => await conn.trans2(lang, 'Video/Audio Tidak ditemukan'))
-    if(!vide) return conn.sendB(m.chat, novid, wm, null, [[await conn.trans(lang, 'Coba Lagi').catch(async _ => await conn.trans2(lang, 'Coba Lagi')), `.play ${text} lainnya`]], m) 
+    if(!vide) return conn.sendB(m.chat, await conn.trans('Video/Audio Tidak ditemukan'), wm, null, [[await conn.trans('Coba Lagi'), `.play ${text} lainnya`]], m) 
     let { authorName, authorAvatar, title, description, url, thumbnail, videoId, durationH, viewH, publishedTime } = vide
     let capt = `🎬 *YouTube Play*
   
@@ -27,7 +21,7 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
 🚀 *Source:* ${url}
 📝 *Description:* ${description}`
     await conn.sendBD(m.chat, capt, wm, img, [['🎧 Audio 🎧', `${usedPrefix}yta ${url}`], ['📽 Video 📽', `${usedPrefix}ytv ${url}`], [`🔎 Play ${await conn.trans(lang, 'Acak').catch(async _ => await conn.trans2(lang, 'Acak'))} 🔍`, `${usedPrefix}playrand ${text}`]], m, {
-     fileName: ucap+` ${m.name} 🤩`, mimetype: global.td, fileLength: global.fsdx, pageCount: global.pcdx,
+     fileName: await conn.trans('Selamat menonton')` ${m.name} 🤩`, mimetype: global.td, fileLength: global.fsdx, pageCount: global.pcdx,
      contextInfo: {
      jpegThumbnail: await(await fetch(thumbd)).buffer(),
      mentionedJid: [m.sender],
@@ -35,7 +29,7 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
      mediaUrl: `${url}`,
      mediaType: 2,
      description: deslink, 
-     title: titlex+'ツ', 
+     title: titlink+'ツ', 
      body: bodlink,
      thumbnail: await(await fetch(thumbnail)).buffer()
      }} 
