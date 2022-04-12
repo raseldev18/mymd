dc182684cc73a1612d4de64f4739da1cec04fe5e
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-04-12 09:47:30 +0700

    Update absen-start.js

--- plugins/absen-start.js
+++ plugins/absen-start.js
@@ -7,18 +7,14 @@ let handler = async (m, { conn, isAdmin, isOwner, usedPrefix, text, participants
     let id = m.chat
     let lang = db.data.users[m.sender].language
     if (id in conn.absen) {
-        let ca = `Masih ada absen di digrup ini`
-        let cb = `Ketik ${usedPrefix}hapusabsen untuk menghapus absen!`
-        let cc = await conn.trans(lang, ca).catch(async _ => await conn.trans2(lang, ca))
-        let cd = await conn.trans(lang, cb).catch(async _ => await conn.trans2(lang, cb)) 
-        let capt = '_*'+cc+'*_\n\n'+cd
-        conn.sendB(m.chat, capt, wm, null, [[await conn.trans(lang, 'Hapus Absen').catch(async _ => await conn.trans2(lang, 'Hapus Absen')), `${usedPrefix}hapusabsen`]], m)
+        let caption = `Masih ada absen di digrup ini!\n\nKetik ${usedPrefix}hapusabsen untuk menghapus absen!`
+        conn.sendB(m.chat, caption, wm, null, [[await conn.trans(lang, 'Hapus Absen'), `${usedPrefix}hapusabsen`]], m)
         throw false
     }
     let hid = await conn.groupMetadata(m.chat)
     let amu = await conn.trans(lang, 'Absen dimulai').catch(async _ => await conn.trans2(lang, 'Absen dimulai'))
     conn.absen[id] = [   
-        conn.sendB(m.chat, amu, wm, null, [[await conn.trans(lang, 'Hadir').catch(async _ => await conn.trans2(lang, 'Hadir')), `${usedPrefix}absen`]], m, {mentions: hid.participants.map(a => a.id)}),
+        conn.sendB(m.chat, amu, wm, null, [[await conn.trans(lang, 'Hadir'), `${usedPrefix}absen`]], m, {mentions: hid.participants.map(a => a.id)}),
         [],
         text
     ]
