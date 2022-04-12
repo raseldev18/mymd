72dffc35a77949442cc4cd7622a05f693f2948a9
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-04-12 09:40:08 +0700

    Update absen-absen.js

--- plugins/absen-absen.js
+++ plugins/absen-absen.js
@@ -3,19 +3,14 @@ let handler = async (m, { conn, usedPrefix }) => {
     let lang = db.data.users[m.sender].language
     conn.absen = conn.absen ? conn.absen : {}
     if (!(id in conn.absen)) {
-        let ca = `Tidak ada absen berlangsung digrup ini`
-        let cb = `Ketik ${usedPrefix}mulaiabsen untuk memulai absen!`
-        let cc = await conn.trans(lang, ca).catch(async _ => await conn.trans2(lang, ca))
-        let cd = await conn.trans(lang, cb).catch(async _ => await conn.trans2(lang, cb)) 
-        let capt = '_*'+cc+'*_\n\n'+cd
-        conn.sendB(m.chat, capt, wm, null, [[await conn.trans(lang, 'Mulai Absen').catch(async _ => await conn.trans2(lang, 'Mulai Absen')), `${usedPrefix}mulaiabsen`]], m)
+        let caption = `_*Tidak ada absen berlangsung digrup ini!*_\n\nKetik ${usedPrefix}mulaiabsen untuk memulai absen!`
+        conn.sendB(m.chat, caption, wm, null, [[await conn.trans(lang, 'Mulai Absen'), `${usedPrefix}mulaiabsen`]], m)
         throw false
     }
     let absen = conn.absen[id][1]
     const wasVote = absen.includes(m.sender)
-    let ud = 'Kamu sudah absen!'
-    let uv = await conn.trans(lang, ud).catch(async _ => await conn.trans2(lang, ud))
-    if(wasVote) return conn.sendB(m.chat, uv, wm, null, [[await conn.trans(lang, 'Cek Absen').catch(async _ => await conn.trans2(lang, 'Cek Absen')), `${usedPrefix}cekabsen`]], m) 
+    let caption1 = 'Kamu sudah absen!'
+    if(wasVote) return conn.sendB(m.chat, caption1, wm, null, [[await conn.trans(lang, 'Cek Absen'), `${usedPrefix}cekabsen`]], m) 
     absen.push(m.sender)
     let d = new Date
     let date = d.toLocaleDateString('id', {
@@ -24,15 +19,15 @@ let handler = async (m, { conn, usedPrefix }) => {
         year: 'numeric'
     })
     let list = absen.map((v, i) => `${gy} ${i + 1}. ${conn.getName(v)}`).join('\n')
-    let caption = `_*${await conn.trans(lang, `Tanggal: ${date}`).catch(async _ => await conn.trans2(lang, `Tanggal: ${date}`))}*_
+    let caption2 = `_*${await conn.trans(lang, `Tanggal: ${date}`)}*_
     ${conn.absen[id][2]}
-${sa}${kki} *${await conn.trans(lang, `Daftar Absen`).catch(async _ =>await conn.trans2(lang, `Daftar Absen`))} ${kka}*
+${sa}${kki} *${await conn.trans(lang, `Daftar Absen`)}*
 ${gz}
-${gx} ${await conn.trans(lang, `Total: ${absen.length}`).catch(async _ => await conn.trans2(lang, `Total: ${absen.length}`))}
+${gx} ${await conn.trans(lang, `Total: ${absen.length}`)}
 ${list}
 ${gz}
 ${sb}`
-    conn.sendB(m.chat, caption, wm, null, [[await conn.trans(lang, 'Hadir').catch(async _ => await conn.trans2(lang, 'Hadir')), `${usedPrefix}absen`], [await conn.trans(lang, 'Cek Absen').catch(async _ => await conn.trans2(lang, 'Cek Absen')), `${usedPrefix}cekabsen`]], m, {mentions: conn.parseMention(caption)})
+    conn.sendB(m.chat, caption2, wm, null, [[await conn.trans(lang, 'Hadir'), `${usedPrefix}absen`], [await conn.trans(lang, 'Cek Absen'), `${usedPrefix}cekabsen`]], m, {mentions: conn.parseMention(caption2)})
 }
 handler.help = ['absent']
 handler.tags = ['absen']
