c5abc89355bbd2b34206622dbd545742bb9e0a11
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-04-12 09:45:13 +0700

    Update absen-delete.js

--- plugins/absen-delete.js
+++ plugins/absen-delete.js
@@ -7,16 +7,12 @@ let handler = async (m, { conn, isAdmin, isOwner, usedPrefix }) => {
     let lang = db.data.users[m.sender].language
     conn.absen = conn.absen ? conn.absen : {}
     if (!(id in conn.absen)) {
-        let ca = `Tidak ada absen berlangsung digrup ini`
-        let cb = `Ketik ${usedPrefix}mulaiabsen untuk memulai absen!`
-        let cc = await conn.trans(lang, ca).catch(async _ => await conn.trans2(lang, ca))
-        let cd = await conn.trans(lang, cb).catch(async _ => await conn.trans2(lang, cb)) 
-        let capt = '_*'+cc+'*_\n\n'+cd
-        conn.sendB(m.chat, capt, wm, null, [[await conn.trans(lang, 'Mulai Absen').catch(async _ => await conn.trans2(lang, 'Mulai Absen')), `${usedPrefix}mulaiabsen`]], m)
+        let caption = `_*Tidak ada absen berlangsung digrup ini!*_\n\nKetik ${usedPrefix}mulaiabsen untuk memulai absen!`
+        conn.sendB(m.chat, caption, wm, null, [[await conn.trans(lang, 'Mulai Absen'), `${usedPrefix}mulaiabsen`]], m)
         throw false
     }
     delete conn.absen[id]
-    m.reply(await conn.trans(lang, 'Absen dihapus').catch(async _ => await conn.trans2(lang, 'Absen dihapus')))
+    m.reply(await conn.trans(lang, 'Absen dihapus'))
 }
 handler.help = ['deleteabsent']
 handler.tags = ['absen']
