0e7a58e437875d02c76aa418f1f40cdb58a6afb4
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-07-30 15:33:20 +0700

    Delete vps.sh

--- vps.sh
+++
@@ -1,8 +0,0 @@
-sudo apt udate && upgrade
-sudo apt install curl
-curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-source ~/.profile
-nvm install nodejs
-sudo apt install ffmpeg
-sudo apt install ffmpeg
-nvm ls-remote
