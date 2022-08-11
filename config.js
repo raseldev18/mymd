b33a6125e9702706a08fd947a8759d1669f4e034
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-08-11 20:37:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,40 +1,12 @@
 let fs = require('fs')
-//============== Data jid =============//
-global.owner = JSON.parse(fs.readFileSync('./src/owner.json')) // Change/add owner number => src/owner.json
-global.mods = JSON.parse(fs.readFileSync('./src/moderator.json')) // Moderator?
-global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Premium user has unlimited limit
-//====== Needed in plugin/update.js ======//
-global.repox = 'raselcomel/RxR-Bot'
-global.branx = 'master'
-//=========== Watermark & Url ==========//
-global.wm = '© rཽᴀˢᵉˡ'
-global.wmtitle = '🌹 𝑪𝒐𝒎𝒆𝒍⋆𝑩𝒐𝒕 ⚚'
-global.wmbody = '🍃 𝑏𝑦 ༆ 𝑟𝑎𝑠𝑒𝑙  ཽ ྀ ྭ ྱ ྲ ཻ' 
-global.wmlink = 'https://www.instagram.com/rasel.ganz'
-global.linkgc = 'https://chat.whatsapp.com/CUCsW6BWfmJLJwJgPQIaKM'
-global.linkmed = 'https://www.instagram.com/p/CH1A1c9J5pY/?utm_medium=copy_link'
-
-//=========== Process Bot ============//
-global.wait = 'Tunggu sebentar ya sayang'
-global.eror = 'Maaf terjadi kesalahan, silahkan coba lagi nanti'
-
-//========= Url Template Button =========//
-global.dTux = 'WEBSITE'
-global.uRlx = 'bit.ly/AcellComel' //''
-//========= Call Template Button =========//
-global.dTcx = 'Add Me'
-global.nUmx = '+62 822-5604-8971' 
-//================ Buttons ================//
-global.bTnx = [['DASHBOARD', '.say blum pasang:v']] //buttons map
-global.mediaMenu = 'https://telegra.ph/file/505b2a03d98bb3b8680e7.jpg'
-global.lIm = 'Ⓛ'
-global.pRm = 'Ⓟ'
-global.uRl = 'Ⓤ'
-global.tXt = 'Ⓣ'
-global.nUm = 'Ⓝ'
-global.qRy = 'Ⓠ'
-global.uSr = '@Ⓤ'
-global.oPs = 'Ⓞ'
+let chalk = reqyire('chalk')
+// global.owner = [
+  // ['6285346545126', 'Acel', true]
+  // [number, dia creator/owner?, dia developer?]
+// ] // Put your number here
+global.owner = JSON.parse(fs.readFileSync('./src/owner.json'))  
+global.mods = JSON.parse(fs.readFileSync('./src/moderator.json')) 
+global.prems = JSON.parse(fs.readFileSync('./src/premium.json'))
 
 global.APIs = { // API Prefix
 //======= name: 'https://website' ========//
@@ -51,54 +23,74 @@ global.APIKeys = { // APIKey Here
 }
 
 //=========== Sticker WM =============//
-const spack = fs.readFileSync("lib/exif.json")
-const stickerpack = JSON.parse(spack)
-if (stickerpack.spackname == '') {
-  var sticker_name = ''
-  var sticker_author = '               ig\n🔥 @rasel.ganz 🔥'
- } else {
-  var sticker_name = stickerpack.spackname
-  var sticker_author = stickerpack.sauthor
+const stick = JSON.parse(readFileSync("lib/exif.json"))
+if (stick.spackname == '' || stick.sauthor == '') {
+  var sticker_name = 'Made with WeA Bot'
+  var sticker_author = '𝖻𝗂𝗍.𝗅𝗒/𝖠𝖼𝖾𝗅𝗅𝖢𝗈𝗆𝖾𝗅'
+} else {
+  var sticker_name = stick.spackname
+  var sticker_author = stick.sauthor
 }
-const file_exif = "lib/exif.json"
-fs.watchFile(file_exif, () => {
-  fs.unwatchFile(file_exif)
-  console.log(chalk.redBright("Update 'exif.json'"))
-  delete require.cache[file_exif]
-  require('./lib/exif.json')
-})
-global.packname = sticker_name
+const weem = JSON.parse(readFileSync("lib/wm.json"))
+if (weem.watermark == '') {
+  var hias_watermark = 'ᴡᴇᴀ ʙᴏᴛ ʙʏ ʀᴀsᴇʟ ⌤'
+} else {
+  var hias_watermark = weem.watermark
+} 
+const symbol = JSON.parse(readFileSync("lib/symbol.json"))
+if (symbol.symbol == '') {
+  var hias_symbol = '•'
+} else {
+  var hias_symbol = symbol.symbol
+} 
+global.packname = sticker_name 
 global.author = sticker_author
-//====== Sticker take(colong stiker:v) ======//
-const cpack = fs.readFileSync("lib/colong.json")
-const ctickerpack = JSON.parse(cpack)
-if (ctickerpack.cpackname == '') {
-  var colong_name = 'sikilinsut olang nylong tikell'
-  var colong_author = 'Ciss 📸\n\n\n\n\n\n\n\n\n\n\n\n@rasel.ganz'
- } else {
-  var colong_name = ctickerpack.cpackname
-  var colong_author = ctickerpack.cauthor
+global.wm = hias_watermark
+global.sYm = hias_symbol
+let list = `Angka
+Asahotak
+Caklontong 
+Family100
+Koboy
+Siapakahaku
+Suitbot
+Susunkata
+Tebakgambar
+Tebakkata
+Tebaklirik
+Tekateki`
+let jsongame = list.split('\n')
+let game = []
+for (let name of jsongame) {
+     game.push({
+       title: name,
+       rowId: '.' + name,
+     })
 }
-const file_colong = "lib/colong.json"
-fs.watchFile(file_colong, () => {
-  fs.unwatchFile(file_colong)
-  console.log(chalk.redBright("Update 'colong.json'"))
-  delete require.cache[file_colong]
-  require('./lib/colong.json')
-})
-global.pclg = colong_name
-global.aclg = colong_author
-
-//======== Fake Size ========//
-global.fsx  = 9999999999999  //fake size foto/video
-global.fsdx = 9999999999999  //fake size document 
-global.pcdx = 9999999999999  // fake page count document
+global.gamelist = {
+    title: 'Silahkan Pilih Game Favorit-mu!',
+    rows: game
+}
 
 global.multiplier = 69 // The higher, The harder levelup
 global.rpg = {
   emoticon(string) {
     string = string.toLowerCase()
     let emot = {
+      name: '🐤',
+      age: '🌴',
+      premium: '🔥',
+      tag: '🐋',
+      number: '✨',
+      wame: '🌹',
+      rank: '🔱',
+      before: '🕊',
+      after: '🦅',
+      atm: '🏧',
+      warning: '⚠️',
+      level: '🧬',
+      limit: '🌌',
+      health: '❤️',
       exp: '✉️',
       money: '💵',
       potion: '🥤',
@@ -108,16 +100,18 @@ global.rpg = {
       mythic: '🗳️',
       legendary: '🗃️',
       pet: '🎁',
-      sampah: '🗑',
+      trash: '🗑',
       armor: '🥼',
       sword: '⚔️',
-      kayu: '🪵',
-      batu: '🪨',
+      wood: '🪵',
+      rock: '🪨',
       string: '🕸️',
-      kuda: '🐎',
-      kucing: '🐈' ,
-      anjing: '🐕',
+      horse: '🐎',
+      cat: '🐈',
+      dog: '🐕',
+      fox: '🦊',
       petFood: '🍖',
+      iron: '⛓️',
       gold: '👑',
       emerald: '💚'
     }
@@ -133,4 +127,12 @@ fs.watchFile(file, () => {
   console.log(chalk.redBright("Update 'config.js'"))
   delete require.cache[file]
   require(file)
+})
+
+let file_exif = "lib/exif.json"
+fs.watchFile(file_exif, () => {
+  fs.unwatchFile(file_exif)
+  console.log(chalk.redBright("Update 'exif.json'"))
+  delete require.cache[file_exif]
+  require('./lib/exif.json')
 })
