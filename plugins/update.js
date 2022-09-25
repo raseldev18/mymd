bacfb70818764ae4fc34dfc2a6a224f800356e8b
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-09-25 16:24:20 +0700

    Update update.js

--- plugins/update.js
+++ plugins/update.js
@@ -1,7 +1,7 @@
 let { execSync } = require('child_process')
 let handler = async (m, { conn, text, isROwner }) => {
   if (conn.user.jid == conn.user.jid) {
-    let stdout = execSync('git remote set-url origin https://github.com/raselcomel/mymd.git && git pull' + (isROwner && text ? ' ' + text : ''))
+    let stdout = execSync('git remote set-url origin ' + set.repo + '.git && git pull' + (isROwner && text ? ' ' + text : ''))
     if (isROwner) require('fs').readdirSync('plugins').map(v => global.reload('', v))
     conn.sendButton(m.chat, stdout.toString(), wm, 0, [['Node Test', '$ node test']], m)
   }
