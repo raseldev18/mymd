29086928ae3cfab39148d62779eae8980a776564
Rasel <90371157+raselcomel@users.noreply.github.com>
2022-09-25 16:26:02 +0700

    Delete plugins directory

--- plugins/cekk.js
+++
@@ -1,7 +0,0 @@
-let handler = async(m, { conn }) => {
-  m.reply('On!')
-}
-handler.customPrefix= /^(te?s|ce?k)$/i
-handler.command = new RegExp
-
-module.exports = handler
--- plugins/stickerOk.js
+++
@@ -1,8 +0,0 @@
-let handler = async(m, { conn }) => {
-  let a = "https://telegra.ph/file/90d392a787612bd894610.png"
-  conn.sendStimg(m.chat, a, m, {packname: packname, author: author })
-}
-handler.customPrefix= /^makasih|kasih|mksh|thank|thanks$/i
-handler.command = new RegExp
-
-module.exports = handler
